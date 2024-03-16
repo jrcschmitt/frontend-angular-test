32faba80577c51cc98e866fcfb775776c73aac82
Junior Carlos Schmitt <junior.schmitt@mouts.info>
2024-03-15 22:28:36 -0300

    Implementação da listagem de filmes paginada e com filtros

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -1,27 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MainService } from '../../services/main.service';
+import { CommonModule } from '@angular/common';
+import { AgGridAngular } from 'ag-grid-angular';
+import {
+  ColDef,
+  GridReadyEvent,
+  IServerSideDatasource,
+  IServerSideGetRowsParams,
+  ValueGetterParams
+} from 'ag-grid-community';
+import 'ag-grid-enterprise';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
+  imports: [
+    CommonModule,
+    AgGridAngular
+  ],
   providers: [MainService],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss'
 })
-export class MovieListComponent implements OnInit {
-  movies: any[] = [];
+export class MovieListComponent {
+  paginationPageSize = 20;
+  columnDefs: ColDef[] = [
+    {
+      headerName: 'ID',
+      field: 'id',
+      sortable: false,
+    },
+    {
+      headerName: 'Year',
+      field: 'year',
+      filter: 'agNumberColumnFilter',
+      filterParams: {
+        buttons: ['reset'],
+        debounceMs: 800,
+        maxNumConditions: 1,
+      },
+      floatingFilter: true,
+      suppressFloatingFilterButton: true,
+      suppressHeaderMenuButton: true,
+      sortable: false,
+    },
+    {
+      headerName: 'Title',
+      field: 'title',
+      flex: 2,
+      sortable: false,
+      minWidth: 180,
+    },
+    {
+      headerName: 'Winner',
+      field: 'winner',
+      filter: true,
+      filterParams: {
+        values: (params: any) => {
+            params.success(['No', 'Yes'])
+        }
+      },
+      floatingFilter: true,
+      sortable: false,
+      valueGetter: (params: ValueGetterParams) => params.data.winner ? 'Yes' : 'No',
+    },
+    {
+      headerName: 'Studios',
+      field: 'studios',
+      sortable: false,
+    },
+    {
+      headerName: 'Producers',
+      field: 'producers',
+      sortable: false,
+    },
+  ];
 
   constructor(private mainService: MainService) {
-
   }
 
-  ngOnInit(): void {
-    this.getMovies();
+  onGridReady(params: GridReadyEvent<any>) {
+    var datasource = this.getServerSideDatasource();
+    params.api!.setGridOption('serverSideDatasource', datasource);
   }
 
-  getMovies() {
-    this.mainService.get('/movies?page=1&size=10').subscribe(res => {
-      this.movies = res.content;
+  getServerSideDatasource(): IServerSideDatasource {
+    return {
+      getRows: (params) => this.getRows(params),
+    };
+  }
+
+  getRows(params: IServerSideGetRowsParams) {
+    const currentPage = params.api.paginationGetCurrentPage();
+    const pageSize = params.api.paginationGetPageSize();
+
+    const filterModel = params.request.filterModel as any;
+
+    const yearFilter = filterModel['year'];
+    const winnerFilter = filterModel['winner'];
+
+    let url = `/movies?page=${currentPage}&size=${pageSize}`;
+
+    if (yearFilter) {
+      url = url + `&year=${yearFilter.filter}`; 
+    }
+
+    if (winnerFilter) {
+      if (winnerFilter.values.length > 0) {
+        const filterValue = winnerFilter.values[0] === 'Yes';
+        url = url + `&winner=${filterValue}`;
+      }
+    }
+
+    this.mainService.get(url)
+    .subscribe({
+      next: res => {
+        params.success({
+          rowData: res.content,
+          rowCount: res.totalElements,
+        });
+      },
+      error: () => params.fail()
     });
   }
 }
